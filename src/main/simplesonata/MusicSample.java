7ab788eae4287db8a36bc2d1e61bdc13bda2429a
Brendan Rollinson <brendan.rollinson@gmail.com>
2018-12-02 15:17:18 -0500

    Implementing initial MusicSample equality check

--- src/main/simplesonata/MusicSample.java
+++ src/main/simplesonata/MusicSample.java
@@ -1,5 +1,7 @@
 package simplesonata;
 
+import java.util.Arrays;
+
 public class MusicSample
 {
     private byte[] samplePoints;
@@ -7,5 +9,20 @@ public class MusicSample
     public MusicSample(byte[] samplePoints)
     {
         this.samplePoints = samplePoints;
+    }
+
+    public byte[] getSamplePoints()
+    {
+        return this.samplePoints;
+    }
+
+    public boolean equals(Object other)
+    {
+        if (!(other instanceof MusicSample))
+        {
+            return false;
+        }
+
+        return Arrays.equals(this.samplePoints, ((MusicSample)other).getSamplePoints());
     }
 }
