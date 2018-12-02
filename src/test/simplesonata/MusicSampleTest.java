6babb9d1799b55804c4886aca10b937e40a58cfc
Brendan Rollinson <brendan.rollinson@gmail.com>
2018-12-02 15:08:12 -0500

    Implementing unit tests for music sample equality

--- src/test/simplesonata/MusicSampleTest.java
+++ src/test/simplesonata/MusicSampleTest.java
@@ -1,5 +1,7 @@
 package simplesonata;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 import org.junit.Test;
 
 public class MusicSampleTest
@@ -7,12 +9,41 @@ public class MusicSampleTest
     @Test
     public void testEqualsReturnsTrueWhenSamplePointsAreTheSame()
     {
-        throw new RuntimeException();
+        MusicSample sample = new MusicSample(new byte[] {
+            1,
+            2
+        });
+        MusicSample otherSample = new MusicSample(new byte[] {
+            1,
+            2
+        });
+
+        assertEquals(otherSample, sample);
     }
 
     @Test
     public void testEqualsReturnsFalseWhenSamplePointsAreNotTheSame()
     {
-        throw new RuntimeException();
+        MusicSample sample = new MusicSample(new byte[] {
+            1,
+            2
+        });
+        MusicSample otherSample = new MusicSample(new byte[] {
+            1,
+            3
+        });
+
+        assertNotEquals(otherSample, sample);
+    }
+
+    @Test
+    public void testEqualsReturnsFalseForObjectWithDifferentClass()
+    {
+        MusicSample sample = new MusicSample(new byte[] {
+            1,
+            2
+        });
+
+        assertNotEquals(new Object(), sample);
     }
 }
