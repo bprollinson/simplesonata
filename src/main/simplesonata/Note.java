cde6ebf872417138e60bb5a70a422864fa9e0766
Brendan Rollinson <brendan.rollinson@gmail.com>
2018-12-01 23:01:44 -0500

    Adding properties to note class and setting up class for representing a note sequence

--- src/main/simplesonata/Note.java
+++ src/main/simplesonata/Note.java
@@ -2,4 +2,12 @@ package simplesonata;
 
 public class Note
 {
+    private int frequencyHZ;
+    private int timeS;
+
+    public Note(int frequencyHZ, int timeS)
+    {
+        this.frequencyHZ = frequencyHZ;
+        this.timeS = timeS;
+    }
 }
---
+++ src/main/simplesonata/NoteSequence.java
@@ -0,0 +1,15 @@
+package simplesonata;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class NoteSequence
+{
+    private List<Note> notes;
+
+    public NoteSequence(Note... notes)
+    {
+        this.notes = new ArrayList<Note>(Arrays.asList(notes));
+    }
+}
