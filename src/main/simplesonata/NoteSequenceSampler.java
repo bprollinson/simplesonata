f5b1248d733c83f581561adf3911949c68aff386
Brendan Rollinson <brendan.rollinson@gmail.com>
2018-12-02 20:04:07 -0500

    Correcting sampling algorithm

--- src/main/simplesonata/NoteSequenceSampler.java
+++ src/main/simplesonata/NoteSequenceSampler.java
@@ -12,8 +12,8 @@ public class NoteSequenceSampler
 
         for (int i = 0; i < samplePoints.length; i++)
         {
-            double periodS = (double)NoteSequenceSampler.SAMPLE_TIME_SECONDS / note.getFrequencyHZ();
-            double angle = 2.0 * Math.PI * i / periodS;
+            double period = (double)samplePointsPerSecond / note.getFrequencyHZ();
+            double angle = 2.0 * Math.PI * i / period;
             samplePoints[i] = (byte)(Math.sin(angle) * 127f);
         }
         MusicSample sample = new MusicSample(samplePoints);
