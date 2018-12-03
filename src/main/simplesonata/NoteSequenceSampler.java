58754f9849d6c8eede20e6d25a4bbf57712310b8
Brendan Rollinson <brendan.rollinson@gmail.com>
2018-12-03 07:10:58 -0500

    Adding unit test for samping intermediate points

--- src/main/simplesonata/NoteSequenceSampler.java
+++ src/main/simplesonata/NoteSequenceSampler.java
@@ -12,9 +12,9 @@ public class NoteSequenceSampler
 
         for (int i = 0; i < samplePoints.length; i++)
         {
-            double period = (double)samplePointsPerSecond / note.getFrequencyHZ();
+            double period = samplePointsPerSecond / note.getFrequencyHZ();
             double angle = 2.0 * Math.PI * i / period;
-            samplePoints[i] = (byte)(Math.sin(angle) * 127f);
+            samplePoints[i] = (byte)(Math.round(Math.sin(angle) * 127f));
         }
         MusicSample sample = new MusicSample(samplePoints);
 
--- src/test/simplesonata/NoteSequenceSamplerTest.java
+++ src/test/simplesonata/NoteSequenceSamplerTest.java
@@ -20,7 +20,7 @@ public class NoteSequenceSamplerTest
     }
 
     @Test
-    public void testSampleReturnsSampleWithIntermediatePoints()
+    public void testSampleReturnsSampleWithMinAndMaxPoints()
     {
         NoteSequenceSampler sampler = new NoteSequenceSampler();
         NoteSequence sequence = new NoteSequence(new Note(1, 1));
@@ -37,5 +37,33 @@ public class NoteSequenceSamplerTest
         });
 
         assertEquals(expectedSample, sampler.sample(sequence, 4));
+    }
+
+    @Test
+    public void testSampleReturnsSampleWithIntermediatePoints()
+    {
+        NoteSequenceSampler sampler = new NoteSequenceSampler();
+        NoteSequence sequence = new NoteSequence(new Note(1, 1));
+
+        MusicSample expectedSample = new MusicSample(new byte[] {
+            0,
+            90,
+            127,
+            90,
+            0,
+            -90,
+            -127,
+            -90,
+            0,
+            90,
+            127,
+            90,
+            0,
+            -90,
+            -127,
+            -90
+        });
+
+        assertEquals(expectedSample, sampler.sample(sequence, 8));
     }
 }
