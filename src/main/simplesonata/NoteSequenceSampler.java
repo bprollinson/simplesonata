5cfac86ac3d0746923fef7b6638b807ced257267
Brendan Rollinson <brendan.rollinson@gmail.com>
2018-12-03 22:38:22 -0500

    Refactoring note sequence sampler

--- src/main/simplesonata/NoteSequenceSampler.java
+++ src/main/simplesonata/NoteSequenceSampler.java
@@ -6,21 +6,34 @@ public class NoteSequenceSampler
 
     public MusicSample sample(NoteSequence sequence, int samplePointsPerSecond)
     {
-        byte[] samplePoints = new byte[samplePointsPerSecond * NoteSequenceSampler.SAMPLE_TIME_S];
-
         Note note = sequence.getNote(0);
+        byte[] samplePoints = this.calculateWaveSampleValues(note, samplePointsPerSecond);
 
-        for (int i = 0; i < samplePoints.length; i++)
-        {
-            double period = samplePointsPerSecond / note.getFrequencyHz();
-            double angle = 2.0 * Math.PI * i / period;
-            samplePoints[i] = (byte)(Math.round(Math.sin(angle) * 127f));
-        }
         MusicSampleNote sampleNote = new MusicSampleNote(samplePoints);
         MusicSample sample = new MusicSample(new MusicSampleNote[] {
             sampleNote
         });
 
         return sample;
+    }
+
+    private byte[] calculateWaveSampleValues(Note note, int samplePointsPerSecond)
+    {
+        byte[] sampleValues = new byte[samplePointsPerSecond * NoteSequenceSampler.SAMPLE_TIME_S];
+
+        for (int timeIndex = 0; timeIndex < sampleValues.length; timeIndex++)
+        {
+            sampleValues[timeIndex] = this.calculateWaveSampleValue(note, samplePointsPerSecond, timeIndex);
+        }
+
+        return sampleValues;
+    }
+
+    private byte calculateWaveSampleValue(Note note, int samplePointsPerSecond, int timeIndex)
+    {
+        double period = samplePointsPerSecond / note.getFrequencyHz();
+        double angle = 2.0 * Math.PI * timeIndex / period;
+
+        return (byte)(Math.round(Math.sin(angle) * 127));
     }
 }
