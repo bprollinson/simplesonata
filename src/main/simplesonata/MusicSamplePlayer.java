adaacb4025d5fdc9d357c9251d5e63d2891fa0a6
Brendan Rollinson <brendan.rollinson@gmail.com>
2018-12-04 21:57:51 -0500

    Cleaning up exception handling in music sample player

--- src/main/simplesonata/MusicSamplePlayer.java
+++ src/main/simplesonata/MusicSamplePlayer.java
@@ -2,11 +2,12 @@ package simplesonata;
 
 import javax.sound.sampled.AudioFormat;
 import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.LineUnavailableException;
 import javax.sound.sampled.SourceDataLine;
 
 public class MusicSamplePlayer
 {
-    public void play(MusicSample sample, int samplePointsPerSecond) throws Exception
+    public void play(MusicSample sample, int samplePointsPerSecond) throws LineUnavailableException
     {
         MusicSampleNote[] sampleNotes = sample.getSampleNotes();
 
@@ -18,7 +19,7 @@ public class MusicSamplePlayer
         this.closeDataLine(dataLine);
     }
 
-    private SourceDataLine openDataLine(int samplePointsPerSecond) throws Exception
+    private SourceDataLine openDataLine(int samplePointsPerSecond) throws LineUnavailableException
     {
         AudioFormat audioFormat = new AudioFormat(samplePointsPerSecond, 8, 1, true, true);
 
@@ -35,7 +36,7 @@ public class MusicSamplePlayer
         dataLine.close();
     }
 
-    private void play(SourceDataLine dataLine, MusicSampleNote sampleNote, int timeMs, int samplePointsPerSecond) throws Exception
+    private void play(SourceDataLine dataLine, MusicSampleNote sampleNote, int timeMs, int samplePointsPerSecond)
     {
         int bufferLength = samplePointsPerSecond * timeMs / 1000;
         int count = dataLine.write(sampleNote.getSamplePoints(), 0, bufferLength);
