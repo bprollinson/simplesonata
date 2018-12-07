1f9541a066f9eb828eed87b9561bb3daa7b6d298
Brendan Rollinson <brendan.rollinson@gmail.com>
2018-12-07 07:16:37 -0500

    Adding sample for fractional note duration

---
+++ src/samples/simplesonata/MultiNoteAndFractionalDurationSample.java
@@ -0,0 +1,21 @@
+package simplesonata;
+
+import javax.sound.sampled.LineUnavailableException;
+
+public class MultiNoteAndFractionalDurationSample implements Sample
+{
+    public void play() throws LineUnavailableException
+    {
+        NoteSequence noteSequence = new NoteSequence(new Note(440, 1), new Note(880, 0.5), new Note(440, 0.5));
+        NoteSequenceSampler sampler = new NoteSequenceSampler();
+
+        MusicSample sample = sampler.sample(noteSequence, 16 * 1024);
+        MusicSamplePlayer player = new MusicSamplePlayer();
+        player.play(sample, 16 * 1024);
+    }
+
+    public static void main(String[] args) throws LineUnavailableException
+    {
+        new MultiNoteAndFractionalDurationSample().play();
+    }
+}
