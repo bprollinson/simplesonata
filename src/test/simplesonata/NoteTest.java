454d561cd691c7bbc4d96e056c1c67c999dd8de2
Brendan Rollinson <brendan.rollinson@gmail.com>
2018-12-08 09:16:34 -0500

    Adding unit tests for creating notes with different letters

--- src/test/simplesonata/NoteTest.java
+++ src/test/simplesonata/NoteTest.java
@@ -28,4 +28,76 @@ public class NoteTest
 
         assertEquals(220, note.getFrequencyHz(), 0.01);
     }
+
+    @Test
+    public void testConstructorSetsFrequencyFromB()
+    {
+        Note note = new Note('B', 3, 1);
+
+        assertEquals(246.94, note.getFrequencyHz(), 0.01);
+    }
+
+    @Test
+    public void testConstructorSetsFrequencyFromC()
+    {
+        Note note = new Note('C', 4, 1);
+
+        assertEquals(261.63, note.getFrequencyHz(), 0.01);
+    }
+
+    @Test
+    public void testConstructorSetsFrequencyFromG()
+    {
+        Note note = new Note('G', 4, 1);
+
+        assertEquals(392.00, note.getFrequencyHz(), 0.01);
+    }
+
+    @Test
+    public void testConstructorSetsFrequencyFromBInLowerOctave()
+    {
+        Note note = new Note('B', 2, 1);
+
+        assertEquals(123.47, note.getFrequencyHz(), 0.01);
+    }
+
+    @Test
+    public void testConstructorSetsFrequencyFromCInLowerOctave()
+    {
+        Note note = new Note('C', 3, 1);
+
+        assertEquals(130.81, note.getFrequencyHz(), 0.01);
+    }
+
+    @Test
+    public void testConstructorSetsFrequencyFromGInLowerOctave()
+    {
+        Note note = new Note('G', 3, 1);
+
+        assertEquals(196, note.getFrequencyHz(), 0.01);
+    }
+
+    @Test
+    public void testConstructorSetsFrequencyFromBInHigherOctave()
+    {
+        Note note = new Note('B', 4, 1);
+
+        assertEquals(493.88, note.getFrequencyHz(), 0.01);
+    }
+
+    @Test
+    public void testConstructorSetsFrequencyFromCInHigherOctave()
+    {
+        Note note = new Note('C', 5, 1);
+
+        assertEquals(523.25, note.getFrequencyHz(), 0.01);
+    }
+
+    @Test
+    public void testConstructorSetsFrequencyFromGInHigherOctave()
+    {
+        Note note = new Note('G', 5, 1);
+
+        assertEquals(783.99, note.getFrequencyHz(), 0.01);
+    }
 }
