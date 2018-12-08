7ff26c63cd75bc1382cc37e3e33a37db1658ccd9
Brendan Rollinson <brendan.rollinson@gmail.com>
2018-12-08 08:53:22 -0500

    Adding sample for playing in different octaves based on note constructor

---
+++ src/samples/simplesonata/AOctaveSample.java
@@ -0,0 +1,21 @@
+package simplesonata;
+
+import javax.sound.sampled.LineUnavailableException;
+
+public class AOctaveSample implements Sample
+{
+    public void play() throws LineUnavailableException
+    {
+        NoteSequence noteSequence = new NoteSequence(new Note('A', 4, 1), new Note('A', 5, 0.5), new Note('A', 3, 0.5));
+        NoteSequenceSampler sampler = new NoteSequenceSampler();
+
+        MusicSample sample = sampler.sample(noteSequence, 16 * 1024);
+        MusicSamplePlayer player = new MusicSamplePlayer();
+        player.play(sample, 16 * 1024);
+    }
+
+    public static void main(String[] args) throws LineUnavailableException
+    {
+        new AOctaveSample().play();
+    }
+}
