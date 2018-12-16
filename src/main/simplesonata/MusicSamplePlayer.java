a8b59b155636c92f1357b10d7abbbba52d4ea863
Brendan Rollinson <brendan.rollinson@gmail.com>
2018-12-16 13:54:49 -0500

    Scaling volume handling to maximum possible volume value

--- src/main/simplesonata/MusicSamplePlayer.java
+++ src/main/simplesonata/MusicSamplePlayer.java
@@ -16,8 +16,9 @@ public class MusicSamplePlayer
         MusicSampleNote[] sampleNotes = sample.getSampleNotes();
 
         SourceDataLine dataLine = this.openDataLine(samplePointsPerSecond);
+        double maxVolumeToStandardVolumeRatio = this.calculateMaxVolumeToStandardVolumeRatio(dataLine);
         for (int i = 0; i < sampleNotes.length; i++) {
-            this.play(dataLine, sampleNotes[i], (int)Math.round(1000 * sampleNotes[i].getDurationInSeconds()), samplePointsPerSecond);
+            this.play(dataLine, sampleNotes[i], (int)Math.round(1000 * sampleNotes[i].getDurationInSeconds()), samplePointsPerSecond, maxVolumeToStandardVolumeRatio);
         }
 
         this.closeDataLine(dataLine);
@@ -34,18 +35,29 @@ public class MusicSamplePlayer
         return dataLine;
     }
 
-    private void play(SourceDataLine dataLine, MusicSampleNote sampleNote, int timeMs, int samplePointsPerSecond)
+    private double calculateMaxVolumeToStandardVolumeRatio(SourceDataLine dataLine)
     {
-        this.setDataLineVolumeFromNote(dataLine, sampleNote);
+        FloatControl volumeControl = (FloatControl)dataLine.getControl(FloatControl.Type.MASTER_GAIN);
+        float maxVolumeDB = volumeControl.getMaximum();
+        double maxVolume = 100 * Math.pow(10.0d, maxVolumeDB / 20.0d);
+        double standardVolumeDB = 20d * Math.log(1d) / Math.log(10d);
+        double standardVolume = 100 * Math.pow(1d, standardVolumeDB / 20.0d);
+
+        return maxVolume / standardVolume;
+    }
+
+    private void play(SourceDataLine dataLine, MusicSampleNote sampleNote, int timeMs, int samplePointsPerSecond, double maxVolumeToStandardVolumeRatio)
+    {
+        this.setDataLineVolumeFromNote(dataLine, sampleNote, maxVolumeToStandardVolumeRatio);
 
         int bufferLength = samplePointsPerSecond * timeMs / 1000;
         int count = dataLine.write(sampleNote.getSamplePoints(), 0, bufferLength);
     }
 
-    private void setDataLineVolumeFromNote(SourceDataLine dataLine, MusicSampleNote sampleNote)
+    private void setDataLineVolumeFromNote(SourceDataLine dataLine, MusicSampleNote sampleNote, double maxVolumeToStandardVolumeRatio)
     {
         FloatControl volumeControl = (FloatControl)dataLine.getControl(FloatControl.Type.MASTER_GAIN);
-        double volumeDB = 20d * Math.log(sampleNote.getVolume() / 100d) / Math.log(10);
+        double volumeDB = 20d * Math.log(sampleNote.getVolume() / 100d * maxVolumeToStandardVolumeRatio) / Math.log(10d);
 
         volumeControl.setValue((float)volumeDB);
     }
