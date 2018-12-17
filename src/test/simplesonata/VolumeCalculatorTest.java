2c2e81a0bb7038aaeeb87ce1aaa9fecbd42cdbad
Brendan Rollinson <brendan.rollinson@gmail.com>
2018-12-17 07:12:50 -0500

    Creating placeholder unit test class for volume calculator

---
+++ src/test/simplesonata/VolumeCalculatorTest.java
@@ -0,0 +1,18 @@
+package simplesonata;
+
+import org.junit.Test;
+
+public class VolumeCalculatorTest
+{
+    @Test
+    public void testCalculateMaxVolumeToStandardVolumeRatio()
+    {
+        throw new RuntimeException();
+    }
+
+    @Test
+    public void testCalculateRealScaledVolumeInDecibels()
+    {
+        throw new RuntimeException();
+    }
+}
